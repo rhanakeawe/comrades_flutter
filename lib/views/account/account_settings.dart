73a31dce5d38af4e340d535c66c72af4db5ef684
RookieSauce <salemauzair@gmail.com>
2024-11-20 14:12:52 -0800

    updated settings page, working on it atm

--- lib/views/account/account_settings.dart
+++ lib/views/account/account_settings.dart
@@ -1,82 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Account_Settings extends StatelessWidget {
+class Account_Settings extends StatefulWidget {
   const Account_Settings({super.key});
 
   @override
+  State<Account_Settings> createState() => _Account_SettingsState();
+}
+
+class _Account_SettingsState extends State<Account_Settings> {
+  @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      heightFactor: 0.9,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 20.0,
-            child: AppBar(
-              toolbarHeight: 30.0,
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text(
-                    'Done',
-                    style: GoogleFonts.roboto(
-                      color: Colors.white,
-                      fontSize: 20,
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: FractionallySizedBox(
+        heightFactor: 0.9,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20.0,
+              child: AppBar(
+                toolbarHeight: 30.0,
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Done',
+                      style: GoogleFonts.roboto(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
                     ),
                   ),
-                ),
-              ],
-              automaticallyImplyLeading: false,
-              backgroundColor: Colors.red,
-            ),
-          ),
-          // Add your settings options here
-          const Expanded(
-              child: Column(
-                children: <Widget> [
-                  Card(
-                      child: ListTile(
-                          title: Text("Account Username")
-                      )
-                  ),
-
-                  Icon(
-                      Icons.person,
-                      color: Colors.green
-                  ),
-                  Text('Account settings', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-
-                  Icon(
-                    Icons.dark_mode,
-                    color: Colors.blue,
-
-                  ),
-                  SizedBox(width:10),
-                  Text('Dark Mode'),
-
-                  Icon(
-                      Icons.logout,
-                      color: Colors.red
-                  ),
-                  Text('Logout'),
-
-
-                  Icon(
-                      Icons.password,
-                      color: Colors.red
-                  ),
-                  Text('Password')
-
                 ],
-              )),
+                automaticallyImplyLeading: false,
+                backgroundColor: Colors.red,
+              ),
+            ),
+            // Add your settings options here
+            Expanded(
+                child: Container(
+                  child: Column(
+                    children: <Widget> [
+                      Icon(
+                          Icons.person,
+                          color: Colors.green
+                      ),
+                      Text('Account settings', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+
+                      Card(
+                          child: ListTile(
+                              title: Text("Account Username"),
+                            leading: CircleAvatar(
+                           //add a background image
+                            )
+                              //need to add a trailing editor
+                          )
+                      ),
+
+
+                      ListTile(
+                        leading: Icon(Icons.lock_outline),
+                        title: Text("Change Password"),
+                        trailing: Icon(Icons.keyboard_arrow_right),
+                        onTap: () {
+                          //change password
+                        },
+                      ),
+                      ListTile(
+                        leading:Icon(Icons.location_on, color: Colors.purple),
+                        title: Text("Change Location"),
+                        trailing: Icon(Icons.keyboard_arrow_right),
+                        onTap: () {
+                          //change password
+                        },
+
+                      ),
 
 
 
 
 
-        ],
+
+                      Icon(
+                          Icons.logout,
+                          color: Colors.red
+                      ),
+                      Text('Logout'),
+
+
+                      Icon(
+                          Icons.password,
+                          color: Colors.red
+                      ),
+                      Text('Password')
+
+                    ],
+                  ),
+                )),
+
+
+
+
+
+          ],
+        ),
       ),
     );
   }
