88552ae3dca97ab7a4b5a8bc8cd509ad1b7c0655
rhanakeawe <rhanakeawe@gmail.com>
2024-12-08 21:32:46 -0800

    groups now instantly changes when database changes (no more scroll to refresh) notifies when group added modified or deleted TODO: redo caching so database isn't called on startup

--- lib/views/groups/groups.dart
+++ lib/views/groups/groups.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:Comrades/data/groupData.dart';
 import 'package:Comrades/data/manageCache.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -20,158 +22,16 @@ class Groups extends StatefulWidget {
 class _GroupsPageState extends State<Groups> {
   FirebaseFirestore db = FirebaseFirestore.instance;
   final QueryList _queryList = QueryList();
-  List<Map<String, dynamic>> groupsList = [];
-
-  @override
-  void initState() {
-    super.initState();
-    getGroups();
-  }
-
-  Future<void> refreshGroups() async {
-    final manageCache = ManageCache();
-    await manageCache.deleteCache('groups_data.json');
-    groupsList.clear();
-    getGroups();
-    NotificationService.showInstantNotification(
-        "Refreshed Groups", "Groups list has been refreshed!");
-  }
-
-  Future<void> getGroups() async {
-    final manageCache = ManageCache();
-    var loadedGroups = await manageCache.loadListFromCache('groups_data.json');
-
-    if (loadedGroups == null) {
-      List<GroupData> groups = [];
-      try {
-        QuerySnapshot<Map<String, dynamic>> groupUserSnapshot =
-            await _queryList.fetchList("groupUserList", "userEmail",
-                FirebaseAuth.instance.currentUser!.email.toString());
-        for (var groupUser in groupUserSnapshot.docs) {
-          QuerySnapshot<Map<String, dynamic>> groupSnapshot = await _queryList
-              .fetchList("groups", "group_ID", groupUser.data()["ID_group"]);
-          for (var group in groupSnapshot.docs) {
-            print(group.data());
-            final gsReference = FirebaseStorage.instance
-                .refFromURL(group.data()["groupPhotoURL"]!);
-            String url = await gsReference.getDownloadURL();
-            setState(() {
-              groups.add(GroupData(
-                  groupName: group.data()["groupName"],
-                  group_ID: group.data()["group_ID"],
-                  groupCreator: group.data()["groupCreator"],
-                  groupDesc: group.data()["groupDesc"],
-                  groupPhotoURL: url,
-                  groupColor: group.data()["groupColor"].toString()));
-            });
-          }
-        }
-        await manageCache.saveListToCache('groups_data.json', groups);
-        for (var group in groups) {
-          setState(() {
-            groupsList.add({
-              "name": group.groupName,
-              "description": group.groupDesc,
-              "backgroundImage": group.groupPhotoURL,
-              "group_ID": group.group_ID,
-            });
-          });
-        }
-      } catch (e) {
-        print("Error getting groups: $e");
-      }
-    } else {
-      loadedGroups = loadedGroups as List<GroupData>;
-      for (var group in loadedGroups) {
-        setState(() {
-          groupsList.add({
-            "name": group.groupName,
-            "description": group.groupDesc,
-            "backgroundImage": group.groupPhotoURL,
-            "group_ID": group.group_ID,
-          });
-        });
-      }
-    }
-  }
-
-  Offset _tapPosition = Offset.zero;
-  void _getTapPosition(TapDownDetails position) {
-    final RenderBox renderBox = context.findRenderObject() as RenderBox;
-    setState(() {
-      _tapPosition = renderBox.globalToLocal(position.globalPosition);
-    });
-  }
-
-  void _showContextMenu(context, Map<String, dynamic> group) async {
-    final RenderObject? overlay =
-        Overlay.of(context).context.findRenderObject();
-    final result = await showMenu(
-        context: context,
-        position: RelativeRect.fromRect(
-            Rect.fromLTWH(_tapPosition.dx, _tapPosition.dy, 10, 10),
-            Rect.fromLTWH(0, 0, overlay!.paintBounds.size.width,
-                overlay.paintBounds.size.height)),
-        items: [
-          const PopupMenuItem(
-            value: "Delete",
-            child: Text('Delete'),
-          )
-        ]);
-    switch (result) {
-      case "Delete":
-        deleteGroup(group["group_ID"], group);
-        break;
-    }
-  }
-
-  Future<void> deleteGroup(String groupID, Map<String, dynamic> group) async {
-    try{
-      QuerySnapshot<Map<String, dynamic>> groupUserListSnapshot =
-      await _queryList.fetchList("groupUserList", "ID_group", group["group_ID"], "userEmail",
-          FirebaseAuth.instance.currentUser!.email.toString());
-      for (var groupUser in groupUserListSnapshot.docs) {
-        print(groupUser.data());
-        db.collection("groupUserList").doc(groupUser.id).delete();
-      }
-    } catch (e) {
-      print("Error deleting groupUser: $e");
-    }
-    groupsList.remove(group);
-    final manageCache = ManageCache();
-    await manageCache.deleteCache('groups_data.json');
-    List<GroupData> groups = [];
-    try {
-      QuerySnapshot<Map<String, dynamic>> groupUserSnapshot =
-          await _queryList.fetchList("groupUserList", "userEmail",
-              FirebaseAuth.instance.currentUser!.email.toString());
-      for (var groupUser in groupUserSnapshot.docs) {
-        QuerySnapshot<Map<String, dynamic>> groupSnapshot = await _queryList
-            .fetchList("groups", "group_ID", groupUser.data()["ID_group"]);
-        for (var group in groupSnapshot.docs) {
-          print(group.data());
-          final gsReference = FirebaseStorage.instance
-              .refFromURL(group.data()["groupPhotoURL"]!);
-          String url = await gsReference.getDownloadURL();
-          setState(() {
-            groups.add(GroupData(
-                groupName: group.data()["groupName"],
-                group_ID: group.data()["group_ID"],
-                groupCreator: group.data()["groupCreator"],
-                groupDesc: group.data()["groupDesc"],
-                groupPhotoURL: url,
-                groupColor: group.data()["groupColor"].toString()));
-          });
-        }
-      }
-      await manageCache.saveListToCache('groups_data.json', groups);
-    } catch (e) {
-      print("Error refreshing group cache after delete: $e");
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
+    bool doNotNotif = true;
+    final String? userEmail = FirebaseAuth.instance.currentUser?.email;
+    final groupUserListStream = db
+        .collection('groupUserList')
+        .where('userEmail', isEqualTo: userEmail)
+        .snapshots();
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
@@ -212,39 +72,120 @@ class _GroupsPageState extends State<Groups> {
           ),
           SizedBox(height: 10),
           Expanded(
-            child: RefreshIndicator(
-              onRefresh: refreshGroups,
-              child: ListView.builder(
-                itemCount: groupsList.length,
-                itemBuilder: (context, index) {
-                  final group = groupsList[index];
-                  return GestureDetector(
-                    onTapDown: (position) {
-                      _getTapPosition(position);
-                    },
-                    onLongPress: () {
-                      _showContextMenu(context, group);
-                    },
-                    child: GroupCard(
-                      name: group['name'],
-                      description: group['description'],
-                      backgroundImage: group['backgroundImage'],
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => GroupsPage(
-                              groupName: group['name'],
-                              groupID: group['group_ID'],
-                              backgroundImage: group['backgroundImage'],
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
+              child: StreamBuilder(
+                  stream: groupUserListStream,
+                  builder: (context, groupUserListSnapshot) {
+                    if (groupUserListSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    if (doNotNotif == false) {
+                      for (var changes in groupUserListSnapshot.data!.docChanges) {
+                        switch (changes.type) {
+                          case DocumentChangeType.added:
+                            NotificationService.showInstantNotification("Added Group", "${changes.doc.data()?["ID_group"]} Added to List!");
+                          case DocumentChangeType.modified:
+                            NotificationService.showInstantNotification("Modified Group", "${changes.doc.data()?["ID_group"]} Modified in List!");
+                          case DocumentChangeType.removed:
+                            NotificationService.showInstantNotification("Removed Group", "${changes.doc.data()?["ID_group"]} Removed from List!");
+                        }
+                      }
+                    }
+                    final groupIDs = groupUserListSnapshot.data?.docs
+                        .map((doc) => doc['ID_group'] as String)
+                        .toList();
+                    final groupsStream = db
+                        .collection('groups')
+                        .where("group_ID", whereIn: groupIDs)
+                        .snapshots();
+                    return StreamBuilder<QuerySnapshot>(
+                        stream: groupsStream,
+                        builder: (context, groupsSnapshot) {
+                          if (groupsSnapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Center(child: CircularProgressIndicator());
+                          }
+                          final groups = groupsSnapshot.data?.docs.map((doc) {
+                            final data = doc.data() as Map<String, dynamic>;
+                            return {
+                              'name': data['groupName'],
+                              'description': data['groupDesc'],
+                              'backgroundImage': data['groupPhotoURL'],
+                              'group_ID': data["group_ID"]
+                            };
+                          }).toList();
+                          doNotNotif = false;
+                          print(doNotNotif);
+                          return ListView.builder(
+                              itemCount: groups?.length,
+                              itemBuilder: (context, index) {
+                                final group = groups?[index];
+                                return FutureBuilder<String?>(
+                                    future: FirebaseStorage.instance
+                                        .refFromURL(group?['backgroundImage'])
+                                        .getDownloadURL(),
+                                    builder: (context, imageSnapshot) {
+                                      final imageUrl = imageSnapshot.data;
+                                      return GestureDetector(
+                                        onTapDown: (TapDownDetails details) async {
+                                          Offset offset = details.globalPosition;
+                                          final left = offset.dx;
+                                          final top = offset.dy;
+                                          await showMenu(
+                                            context: context,
+                                            position: RelativeRect.fromLTRB(left, top, 100, 100),
+                                            items: [
+                                              const PopupMenuItem(
+                                                value: "Delete",
+                                                child: Text('Delete'),
+                                              )]
+                                          ).then((value) async {
+                                            switch (value) {
+                                              case "Delete": {
+                                                try {
+                                                  QuerySnapshot<Map<String, dynamic>>
+                                                  groupUserListSnapshot =
+                                                      await _queryList.fetchList(
+                                                      "groupUserList",
+                                                      "ID_group",
+                                                      group?["group_ID"],
+                                                      "userEmail",
+                                                      FirebaseAuth.instance
+                                                          .currentUser!.email
+                                                          .toString());
+                                                  for (var groupUser
+                                                  in groupUserListSnapshot.docs) {
+                                                    print(groupUser.data());
+                                                    db.collection("groupUserList")
+                                                        .doc(groupUser.id)
+                                                        .delete();}
+                                                } catch (e) {
+                                                  print("Error deleting groupUser: $e");}
+                                                break;
+                                              }}});},
+                                        child: GroupCard(
+                                          name: group?['name'],
+                                          description: group?['description'],
+                                          backgroundImage: imageUrl ?? "",
+                                          onTap: () {
+                                            Navigator.of(context).push(
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    GroupsPage(
+                                                  groupName: group?['name'],
+                                                  groupID: group?['group_ID'],
+                                                  backgroundImage:
+                                                      imageUrl ?? "",
+                                                ),
+                                              ),
+                                            );
+                                          },
+                                        ),
+                                      );
+                                    });
+                                });
+                          });
+                  })
           ),
         ],
       ),
